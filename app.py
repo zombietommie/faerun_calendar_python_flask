f3d4a37c4b2605571675b7827fcb473d3c4098a5
Tommy Tran <zombietommie@gmail.com>
2025-07-21 23:13:31 -0500

    init project

---
+++ app.py
@@ -0,0 +1,38 @@
+from flask import Flask, render_template
+import calendar_logic # import the calendar logic
+
+# Create an instance of the Flask class
+app = Flask(__name__)
+
+# Define a route for the home page
+@app.route("/")
+def index():
+
+    all_months = calendar_logic.MONTHS
+
+    return render_template(
+        'index.html',
+        months=all_months,
+    )
+
+@app.route("/month/<month_name>")
+def month_view(month_name):
+    # Find the month's number (index + 1)
+    try:
+        month_number = calendar_logic.MONTHS.index(month_name) + 1
+    except ValueError:
+        # If the month name isn't valid, return a 404 error
+        return "Month no found!", 404
+
+    # Check for a holiday after the previous month
+    holiday_before = calendar_logic.get_holiday_after_month(month_number - 1)
+
+    # check for holiday after the current month
+    holiday_after = calendar_logic.get_holiday_after_month(month_number)
+
+    return render_template(
+        "month_view.html",
+        name=month_name,
+        holiday_before=holiday_before,
+        holiday_after=holiday_after
+    )
---
+++ calendar_logic.py
@@ -0,0 +1,38 @@
+# A dictionary to hold the special holidays that fall between months.
+# The key is the month number *before* the holiday.
+# For example Midwinter is between month 12 and 1.
+
+HOLIDAYS = {
+        0: "Midwinder",
+        3: "Greengrass",
+        6: "Midsummer",
+        9: "Highharvestide",
+        12: "The Feast of the Moon",
+}
+
+# A list of the 12 months, in order.
+MONTHS = [
+        "Hammer",
+        "Alturiak",
+        "Ches",
+        "Tarsakh",
+        "Mirtul",
+        "Kythorn",
+        "Flamerule",
+        "Eleasis",
+        "Eleint",
+        "Marpenoth",
+        "Uktar",
+        "Nightal",
+]
+
+def get_month_name(month_number):
+    """Returns the name of a month from its number (1-12)."""
+    if 1 <= month_number <= 12:
+        return MONTHS[month_number -1]
+    return None
+
+def get_holiday_after_month(month_number):
+    """Returns the name of a holiday following a given month number."""
+    return HOLIDAYS.get(month_number)
+
